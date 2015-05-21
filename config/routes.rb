e9fa20f5903f55abf0516fef85fabd62d5d9e19f
Kristin Albert <kalbert@circle.com>
2015-05-21 14:03:04 +0200

    Adds multilanguage support

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :tasks
 
+  scope "(:locale)", locale: /en|de/ do
+    resources :tasks
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'tasks#index'
+  get '/:locale' => 'tasks#index'
+
    #get 'contact' => 'layouts#contact'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
